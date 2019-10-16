c0824ba0df810d6b0f2fb8f70d613a802d0cd8a9
bavokn <bknaeps@gmail.com>
2019-10-17 00:44:27 +0200

    done

--- Algorithm.py
+++ Algorithm.py
@@ -1,83 +1,21 @@
+# coding=utf-8
 import cv2
 import numpy as np
 
 files = {}
 contourData = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: []}
-coins = {0.01: 0,  0.02: 0, 0.05: 0, 0.1: 0, 0.2: 0, 0.5: 0, 1: 0, 2: 0}
+coins = {0.01: 0, 0.02: 0, 0.05: 0, 0.1: 0, 0.2: 0, 0.5: 0, 1: 0, 2: 0}
 
 kernel = np.ones((5, 5), np.uint8)
-#80,135,225
+# 80,135,225
 lower = np.array([0, 80, 100])
 higher = np.array([255, 255, 255])
 image = None
 
-for i in range(9):
-    #create mask for the image , filtering out most of the blue
-    image = cv2.imread("./data/P1000" + str(697 + i) + "s.jpg")
-    mask = cv2.inRange(image, lower, higher)
-    image = cv2.bitwise_and(image, image, mask=mask)
-
-    #grey scale the image
-    greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #set the treshold so that only coins are fully visible
-    ret, thresh = cv2.threshold(greyscale, 90, 255, cv2.THRESH_BINARY)
-
-    # noise removal
-    kernel = np.ones((3, 3), np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-    #for creating less whitespaces
-    erosion = cv2.erode(opening, kernel, iterations=6)
-
-    # sure background area
-    dilate = cv2.dilate(erosion, kernel, iterations=1)
-
-    blur = cv2.blur(dilate, (3, 3))
-
-    # Set our filtering parameters
-    # Initialize parameter setting using cv2.SimpleBlobDetector
-    params = cv2.SimpleBlobDetector_Params()
-
-    # Set Area filtering parameters
-    params.filterByArea = True
-    params.minArea = 5000
-    params.maxArea = 300000
-
-    # Set Circularity filtering parameters
-    params.filterByCircularity = True
-    params.minCircularity = 0.8
-
-    # Set Convexity filtering parameters
-    params.filterByConvexity = True
-    params.minConvexity = 0.2
-
-    # Set inertia filtering parameters
-    params.filterByInertia = True
-    params.minInertiaRatio = 0.01
-
-    # Create a detector with the parameters
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    #invert image for blob detection
-    invert = cv2.bitwise_not(blur)
-    # Detect blobs
-    keypoints = detector.detect(invert)
-    print len(keypoints)
-    # Draw blobs on our image as red circles
-    blank = np.zeros((1, 1))
-    files[i] = cv2.drawKeypoints(invert, keypoints, blank, (0, 0, 255),
-                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    tmp = []
-    for key in keypoints:
-        tmp.append(key.size)
-        tmp.sort()
-        contourData[i].append(key.size)
-
-cv2.imshow("test", files[8])
-#
-# for i in files:
-#     cv2.imshow(str(i), files[i])
-
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+fontScale              = 20
+fontColor              = (255, 255, 255)
+lineType               = 20
 
 def amount(array):
     total = 0
@@ -109,16 +47,85 @@ def amount(array):
 
     return total
 
+for i in range(9):
+    # create mask for the image , filtering out most of the blue
+    orgImage = cv2.imread("./data/P1000" + str(697 + i) + "s.jpg")
+    mask = cv2.inRange(orgImage, lower, higher)
+    image = cv2.bitwise_and(orgImage, orgImage, mask=mask)
+
+    # grey scale the image
+    greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # set the treshold so that only coins are fully visible
+    ret, thresh = cv2.threshold(greyscale, 90, 255, cv2.THRESH_BINARY)
+
+    # noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+    # for creating less whitespaces
+    erosion = cv2.erode(opening, kernel, iterations=6)
+
+    # sure background area
+    dilate = cv2.dilate(erosion, kernel, iterations=1)
+
+    blur = cv2.blur(dilate, (3, 3))
+
+    # Set our filtering parameters
+    # Initialize parameter setting using cv2.SimpleBlobDetector
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Set Area filtering parameters
+    params.filterByArea = True
+    params.minArea = 5000
+    params.maxArea = 300000
+
+    # Set Circularity filtering parameters
+    params.filterByCircularity = True
+    params.minCircularity = 0.8
+
+    # Set Convexity filtering parameters
+    params.filterByConvexity = True
+    params.minConvexity = 0.2
+
+    # Set inertia filtering parameters
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.01
+
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # invert image for blob detection
+    invert = cv2.bitwise_not(blur)
+    # Detect blobs
+    keypoints = detector.detect(invert)
+    print len(keypoints)
+    # Draw blobs on our image as red circles
+    blank = np.zeros((1, 1))
+
+
+
+    tmp = []
+    for key in keypoints:
+        tmp.append(key.size)
+        tmp.sort()
+        val = amount(tmp)
+        x, y = key.pt
+        bottomLeftCornerOfText = (int(round(x)), int(round(y)))
+        contourData[i].append(key.size)
+        files[i] = cv2.putText(orgImage, str(val), bottomLeftCornerOfText, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255),
+                               lineType=cv2.LINE_AA)
+        tmp = []
+    files[i] = cv2.drawKeypoints(orgImage, keypoints, blank, (0, 0, 255),
+                                 cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+i = 0
+for file in files:
+    cv2.imshow(str(i), files[file])
+    i += 1
+
 for data in contourData:
     contourData[data].sort()
     print contourData[data]
     print amount(contourData[data])
     print ("----------------------------")
-
-
-
-#draw all the original images with the ellipses
-# for i in files:
-#     cv2.imshow("test" + str(i), files[i])
 
 cv2.waitKey()
